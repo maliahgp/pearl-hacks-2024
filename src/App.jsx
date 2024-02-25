b9f60effdc9800274baaa442c3351ae6fcfde53c
maliahgp <maliahgp@ad.unc.edu>
2024-02-24 19:18:59 -0500

    Brand new layout with a little functionality

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { UNCMap } from './components/Map'
+
+
+function App() {
+  const [activeLocation, setActiveLocation] = useState({})
+
+  const updateActiveLocation = (location) => {
+    setActiveLocation(location);
+  };
+
+  return (
+    <>
+     <div className="container">
+      <div className='container-left'>
+      <UNCMap markerClick={updateActiveLocation} ></UNCMap>
+      </div>
+      <div className='container-right'>
+        <div className='location-pic'>
+          {activeLocation.picture}
+        </div>
+        <div className='comments'></div>
+      </div>
+     </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Map.jsx
@@ -0,0 +1,17 @@
+import { Map, Marker } from "pigeon-maps"
+import LenoirPic from '../assets/Lenoir-Pic.jpg'
+
+export function UNCMap({markerClick}) {
+    const places = {
+        Lenoir: {name: "Lenoir", coords: [35.910651317144364, -79.0486821489566], picture: <img src={LenoirPic}></img>}
+    }
+
+    return (
+    <Map defaultCenter={[35.91056, -79.05021]} defaultZoom={11}>
+        {/* <Marker width={50} anchor={[35.91056, -79.05021]} /> */}
+        {Object.keys(places).map((placeName) => {
+            return <Marker key={places[placeName].name} anchor={places[placeName].coords} onClick={() => markerClick(places[placeName])}></Marker>
+        })}
+      </Map>
+    )
+}
